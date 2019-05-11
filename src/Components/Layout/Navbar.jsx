c1654c5637eb6085d6080f1d463cb794f982bc08
Ghiorghiu Ioan-Viorel <logoeje@gmail.com>
2019-05-11 17:10:22 +0300

    Added Navbar

---
+++ src/Components/Layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function Navbar() {
+	return (
+		<div>
+			Header
+		</div>
+	);
+}
+
+export default Navbar;
