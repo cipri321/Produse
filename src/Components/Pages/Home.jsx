211269b6fbe2c991bc0bc0cbb2b1653bae82050d
Cipri <ciprimarin2001@gmail.com>
2019-05-11 17:30:59 +0300

    added the home page

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Home() {
+    return (
+      <div>
+        Bine ati venit la aplicatia noastra care poate adauga si sterge produse dintr-o lista.
+      <div>
+    );
+}
